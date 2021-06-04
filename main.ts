e66fb4e28b6db0ddff43e74efa681d46e610be29
Tarosa <tarosa.y@gmail.com>
2021-06-04 18:44:14 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,13 @@ bluetooth.onBluetoothConnected(function () {
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Happy)
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Angry)
+    pins.servoWritePin(AnalogPin.P1, 26)
+    basic.pause(480)
+    pins.servoWritePin(AnalogPin.P1, 100)
+    basic.showIcon(IconNames.Happy)
+})
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
     if (cmd == "f") {
@@ -14,9 +21,16 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
         bluetooth.uartWriteString("OK")
     }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Angry)
+    pins.servoWritePin(AnalogPin.P1, 26)
+    basic.pause(480)
+    pins.servoWritePin(AnalogPin.P1, 100)
+    basic.showIcon(IconNames.Happy)
+})
 let cmd = ""
 bluetooth.startUartService()
-led.setBrightness(20)
+led.setBrightness(10)
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
 	
